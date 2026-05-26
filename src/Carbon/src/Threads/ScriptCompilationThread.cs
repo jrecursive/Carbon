534d2818b19aed68a9c93ee7095daa95be2e961d
jrecursive <jmuellerleile@gmail.com>
2026-05-26 06:21:46 -0500

    Fix Cecil resolver and staging install workflow

--- src/Carbon/src/Threads/ScriptCompilationThread.cs
+++ src/Carbon/src/Threads/ScriptCompilationThread.cs
@@ -252,10 +252,15 @@ public class ScriptCompilationThread : BaseThreadedJob
 
 		public override AssemblyDefinition Resolve(AssemblyNameReference name)
 		{
-			return Resolve(name, new ReaderParameters());
+			return ResolveAssembly(name, new ReaderParameters { AssemblyResolver = this });
 		}
 
 		public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+		{
+			return ResolveAssembly(name, parameters);
+		}
+
+		private AssemblyDefinition ResolveAssembly(AssemblyNameReference name, ReaderParameters parameters)
 		{
 			if (cache.TryGetValue(name.FullName, out var assembly))
 				return assembly;
@@ -308,7 +313,7 @@ public class ScriptCompilationThread : BaseThreadedJob
 		protected override void Dispose(bool disposing)
 		{
 			foreach (var assembly in cache.Values)
-				assembly.Dispose();
+				assembly?.Dispose();
 
 			cache.Clear();
 
